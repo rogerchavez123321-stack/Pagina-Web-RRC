ebe2c96f38edf193f60265e6126c513bf32c370a
Thomas Avila <thomasteagua123@gmail.com>
2026-08-24 16:42:06 -0300

    completo

--- Pagina-RRC/inicio-react/src/App.jsx
+++ Pagina-RRC/inicio-react/src/App.jsx
@@ -1,37 +1,45 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-import Persona from './components/Persona'
-import Perfil from './components/Perfil'
-import TarjetaPelicula from './components/TarjetaPelicula'
-import Contador from './components/Contador'
-
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+import Persona from "./components/Persona";
+import Perfil from "./components/Perfil";
+import TarjetaPelicula from "./components/TarjetaPelicula";
+import Contador from "./components/Contador";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const peliculas = [
-  { id: 1, titulo: "Interstellar",año: 2014, vista: false },
-  { id: 2, titulo: "The Dark Knight",año: 2008, vista: true  },
-  { id: 3, titulo: "Inception",año: 2010, vista: false },
-  { id: 4, titulo: "Oppenheimer",año: 2023, vista: true  },
+    { id: 1, titulo: "Interstellar", año: 2014, vista: false },
+    { id: 2, titulo: "The Dark Knight", año: 2008, vista: true },
+    { id: 3, titulo: "Inception", año: 2010, vista: false },
+    { id: 4, titulo: "Oppenheimer", año: 2023, vista: true },
   ];
   return (
     <>
-     <div>
-      <Perfil nombre="Roger" rol="Desarrollador" lenguajes={["JavaScript", "React", "CSS"]} />
-     </div>
-     <Persona nombre="Roger" apellido="Chavez"/>
-     <div>
-      <h2>Lista de Películas</h2>
-      {peliculas.map((peli) => (
-        <TarjetaPelicula key={peli.id} titulo={peli.titulo} año={peli.año} vista={peli.vista} />
-      ))}
-     </div>
-     <div>
-      <Contador />
-     </div> 
+      <div>
+        <Perfil
+          nombre="Roger"
+          rol="Desarrollador"
+          lenguajes={["JavaScript", "React", "CSS"]}
+        />
+      </div>
+      <Persona nombre="Roger" apellido="Chavez" />
+      <div>
+        <h2>Lista de Películas</h2>
+        {peliculas.map((peli) => (
+          <TarjetaPelicula
+            key={peli.id}
+            titulo={peli.titulo}
+            año={peli.año}
+            vista={peli.vista}
+          />
+        ))}
+      </div>
+      <div>
+        <Contador />
+      </div>
 
       <section id="center">
         <div className="hero">
@@ -53,10 +61,8 @@ function App() {
           Count is {count}
         </button>
       </section>
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
